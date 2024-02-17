1e346db72f8fcf69578cca555bbe2ec1d680df37
Mohab Sherif <mohab.barca@gmail.com>
2024-02-17 19:44:29 +0200

    feat: Added  UI to the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import TabsNavigation from './components/ui/TabsNavigation'
 
 function App() {
 
 
   return (
     <>
+      <div className="flex flex-col bg-white  h-screen">
+        <div className="w-full  py-4 px-8   border-b">
+          <h1 className="text-[32px] text-black font-bold">
+            /r/FlutterDev
+          </h1>
+        </div>
+
+        <TabsNavigation />
+      </div>
 
     </>
   )
